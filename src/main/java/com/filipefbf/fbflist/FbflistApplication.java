dec968d2d8e2d4a36f82d01733c4e58901883582
Filipe <filipe_fbf@hotmail.com>
2023-07-12 21:42:30 -0300

    Project created

---
+++ src/main/java/com/filipefbf/fbflist/FbflistApplication.java
@@ -0,0 +1,13 @@
+package com.filipefbf.fbflist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbflistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbflistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipefbf/fbflist/FbflistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipefbf.fbflist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbflistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
